6986e575547def3e55e3cec6d0e740c1c9796ba6
michdol <michal.dolbniak@gmail.com>
2025-10-22 08:32:09 +0200

    a

--- init.lua
+++ init.lua
@@ -475,7 +475,7 @@ require('lazy').setup({
           -- Jump to the definition of the word under your cursor.
           --  This is where a variable was first declared, or where a function is defined, etc.
           --  To jump back, press <C-t>.
-          -- map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
           map('gv', ':vsplit | lua vim.lsp.buf.definition()<CR>', '[G]oto [D]efinition')
 
